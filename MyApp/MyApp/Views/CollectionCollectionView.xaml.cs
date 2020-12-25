5f003aa8d0e61df4372da43171ec76fbff3359a1
drsmile <drsmile.1001@gmail.com>
2020-12-25 16:21:02 +0800

    feat: 加入靜態集合的 CollectionView 範例

---
+++ MyApp/MyApp/Views/CollectionCollectionView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionCollectionView : ContentPage
+    {
+        public class ValueModel
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        public CollectionCollectionView()
+        {
+            InitializeComponent();
+            BindingContext = new
+            {
+
+                Items = Enumerable.Range(0, 100).Select(i => new ValueModel
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Age = i
+                }).ToArray()
+            };
+        }
+    }
+}
