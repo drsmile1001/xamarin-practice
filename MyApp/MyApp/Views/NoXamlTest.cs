cf250b863be0e1026426df5b5c852e309351120d
drsmile <drsmile.1001@gmail.com>
2020-12-29 09:54:13 +0800

    feat: 嘗試不使用 XAML 實作頁面

---
+++ MyApp/MyApp/Views/NoXamlTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+using ReactiveUI;
+
+
+namespace MyApp.Views
+{
+    public class NoXamlTest : ContentPage
+    {
+        public class State : ReactiveObject
+        {
+            private int _count;
+            public int Count
+            {
+                get => _count;
+                set => this.RaiseAndSetIfChanged(ref _count, value);
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var state = new State();
+
+            var counterLabel = new Label { Text = $"Clicked 0 times！" };
+            state.WhenAnyValue(s => s.Count)
+                .Subscribe(count =>
+                {
+                    counterLabel.Text = $"Clicked {count} times！";
+                });
+
+            var addButton = new Button
+            {
+                Text = "Add",
+                Command = new Command(() =>
+                {
+                    state.Count += 2;
+                })
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    counterLabel,
+                    addButton
+                }
+            };
+        }
+    }
+}
